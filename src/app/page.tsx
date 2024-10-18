b662d5ff7f1d68af4b1b6a20d329df136f946b9e
Areeba Sadiq <areebasadiq41@gmail.com>
2024-10-18 19:48:20 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,14 +14,14 @@ export default function Home() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 1000); // Adjust the loading time if needed
+    }, 1000); 
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <div className="relative flex h-screen"> {/* Set height to full screen */}
-      {loading && <Loader />} {/* Loader will cover the screen */}
+    <div className="relative flex h-screen"> 
+      {loading && <Loader />} 
       <div className={`flex w-full transition-opacity duration-500 ${loading ? 'opacity-0' : 'opacity-100'}`}>
         <Sidebar />
         <div className="w-full">
