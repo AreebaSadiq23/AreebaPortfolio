263db9528cb2cc4998ce5429341c12efd790d976
AreebaSadiq23 <areebasadiq41@gmail.com>
2024-10-19 19:08:53 +0500

    Update contact

--- src/app/components/contact.tsx
+++ src/app/components/contact.tsx
@@ -20,7 +20,8 @@ const DecorativeDots: React.FC = () => {
 const Contact: React.FC = () => {
   return (
     <section id="contact" className="bg-gray-900 py-12">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-start px-6">
+      <div className="container mx-auto flex flex-col md:flex-row justify-center md:justify-between items-start px-6">
+        {/* Contact Info */}
         <div className="contact-info flex-1 p-8 mb-8 md:mb-0 justify-center mt-16">
           <div className="info text-gray-400">
             <p className="mb-6 flex items-center">
@@ -52,20 +53,20 @@ const Contact: React.FC = () => {
         </div>
 
         {/* Contact Form */}
-        <div className="contact-form flex-1 bg-gray-800 p-8 rounded-lg shadow-lg">
-          <h2 className="text-4xl font-bold mb-4 text-teal-600">
+        <div className="contact-form flex-1 bg-gray-800 p-8 rounded-lg shadow-lg mx-auto">
+          <h2 className="text-4xl font-bold mb-4 text-teal-600 text-center">
             Get in Touch
           </h2>
           <form>
             <div className="form-group mb-4 flex flex-col md:flex-row md:space-x-4">
-              <div className="flex-1">
+              <div className="flex-1 mb-4 md:mb-0">
                 <input
                   type="text"
                   id="firstName"
                   name="firstName"
                   placeholder="Your First Name"
                   required
-                  className="w-full border border-gray-300 rounded p-3 focus:outline-none focus:ring-2 focus:ring-blue-600 bg-transparent placeholder-light-gray mb-4"
+                  className="w-full border border-gray-300 rounded p-3 focus:outline-none focus:ring-2 focus:ring-blue-600 bg-transparent placeholder-light-gray"
                 />
               </div>
               <div className="flex-1">
@@ -75,7 +76,7 @@ const Contact: React.FC = () => {
                   name="lastName"
                   placeholder="Your Last Name"
                   required
-                  className="w-full border border-gray-300 rounded p-3 focus:outline-none focus:ring-2 focus:ring-blue-600 bg-transparent placeholder-light-gray mb-4"
+                  className="w-full border border-gray-300 rounded p-3 focus:outline-none focus:ring-2 focus:ring-blue-600 bg-transparent placeholder-light-gray"
                 />
               </div>
             </div>
@@ -87,7 +88,7 @@ const Contact: React.FC = () => {
                 name="email"
                 placeholder="Your Email"
                 required
-                className="w-full border border-gray-300 rounded p-3 focus:outline-none focus:ring-2 focus:ring-blue-600 bg-transparent placeholder-light-gray mb-4"
+                className="w-full border border-gray-300 rounded p-3 focus:outline-none focus:ring-2 focus:ring-blue-600 bg-transparent placeholder-light-gray"
               />
             </div>
             <div className="form-group mb-4">
@@ -97,12 +98,12 @@ const Contact: React.FC = () => {
                 rows={4}
                 placeholder="Your Message"
                 required
-                className="w-full border border-gray-300 rounded p-3 focus:outline-none focus:ring-2 focus:ring-blue-600 bg-transparent placeholder-light-gray mb-4"
+                className="w-full border border-gray-300 rounded p-3 focus:outline-none focus:ring-2 focus:ring-blue-600 bg-transparent placeholder-light-gray"
               />
             </div>
             <button
               type="submit"
-              className="bg-cyan-700 text-white py-3 px-6 rounded-lg border border-cyan-500 flex items-center hover:bg-cyan-500 transition duration-300"
+              className="bg-cyan-700 text-white py-3 px-6 rounded-lg border border-cyan-500 flex items-center justify-center mx-auto hover:bg-cyan-500 transition duration-300"
             >
               <AiOutlineArrowUp size={20} className="mr-2" />
               <span>Send Message</span>
