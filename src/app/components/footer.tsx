4849a47ea74bc45392596fc8ddae1df73845e5fd
AreebaSadiq23 <areebasadiq41@gmail.com>
2025-04-04 19:57:45 +0500

    update footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,4 +1,4 @@
-import { FaGithub, FaDiscord } from "react-icons/fa";
+import { FaGithub, FaDiscord, FaLinkedin } from "react-icons/fa";
 
 const Footer: React.FC = () => {
   const currentYear = new Date().getFullYear();
@@ -32,6 +32,17 @@ const Footer: React.FC = () => {
               className="hover:text-white transition duration-300"
             />
           </a>
+          <a
+            href="https://www.linkedin.com/in/areeba-sadiq-934a09351"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin
+              size={40}
+              className="hover:text-white transition duration-300"
+            />
+          </a>
+
         </div>
       </div>
     </footer>
